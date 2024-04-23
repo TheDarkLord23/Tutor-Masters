77ccfec97a0cbf5a375b47c35879e8f0f3ed71de
Anna Hempel <annahempel@gmx.net>
2024-04-23 14:11:43 +0200

    CRUD Courses finished

--- admin/deleteCourses.php
+++ admin/deleteCourses.php
@@ -1,3 +1,39 @@
+<?php 
+
+require_once "../db_connection.php";
+
+session_start();
+
+if (!isset($_SESSION["admin"]) && !isset($_SESSION["trainer"])) {
+    header("Location: ../login.php");
+}
+
+if (isset($_SESSION["user"])) {
+    header("Location: ../User/indexUser.php");
+}
+
+
+$id = $_GET["id"];
+
+$sql = "SELECT * FROM `courses` WHERE id = $id";
+$result = mysqli_query($connection, $sql);
+$row = mysqli_fetch_assoc($result);
+if ($row["picture"] != "defaultPic.jpg") {
+    unlink("../Images/{$row["picture"]}");
+}
+
+$delete = "DELETE FROM `courses` WHERE id = $id";
+
+if(mysqli_query($connection, $delete)){
+    header("Location: indexAdmin.php");
+}else {
+    echo "Error";
+}
+
+mysqli_close($connection);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +42,7 @@
     <title>Document</title>
 </head>
 <body>
+
     
 </body>
 </html>
