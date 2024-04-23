30f0e59ce50d769090871477373f3299a6b222cd
Anna Hempel <annahempel@gmx.net>
2024-04-23 10:58:24 +0200

    create courses admin

--- login/login.php
+++ login/login.php
@@ -78,7 +78,6 @@ if (isset($_SESSION["trainer"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../Style/loginRegister.css">
 </head>
 
 <body>
