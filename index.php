50ef92bd8354d6c080ef36f06e7939c898268777
Alex <alex23antonov@gmail.com>
2024-04-24 14:08:52 +0200

    index and imgs

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 require_once "db_connection.php";
 require_once "navbar.php";
 
-$readQuery = "SELECT * from courses";
+$readQuery = "SELECT * FROM courses
+ORDER BY `date` ASC LIMIT 4";
 $readResult = mysqli_query($connection, $readQuery);
+
 
 $layout = "";
 
@@ -14,17 +16,16 @@ if (mysqli_num_rows($readResult) == 0) {
 } else {
     $rows = mysqli_fetch_all($readResult, MYSQLI_ASSOC);
     foreach ($rows as $value) {
-        $layout .= "<div class='center'>
-        <div class='card' style='width: 18rem;'>
-        <img src='img/{$value["picture"]}' class='card-img-top' alt='...'>
-        <div class='card-body'>
-          <h5 class='card-title'>{$value["subject"]}</h5>
-          <p class='card-text'>Date:{$value["date"]}</p>
-          <p class='card-text'>Duration:{$value["duration"]}</p>
-          
-          <a href='details.php?id={$value["id"]}' class='btn btn-success'>Details</a>
-      </div>
-      </div>
+        $layout .= "
+        <div class='card-holder'>
+            <img src='img/{$value["picture"]}' alt='Image description' />
+            <h4>{$value["subject"]}</h4>
+            <p>
+            Date:{$value["date"]}
+            </p>
+            <p>
+            Duration:{$value["duration"]}
+            </p><a href='details.php?id={$value["id"]}' class='card-btn'>Details</a>
         </div>";
     }
 }
@@ -32,6 +33,7 @@ if (mysqli_num_rows($readResult) == 0) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 
 <head>
     <meta charset="UTF-8">
@@ -42,179 +44,96 @@ if (mysqli_num_rows($readResult) == 0) {
 </head>
 
 <body>
-
-
-
-    <div class="med-related-prod-wrap" id="med-related-prod-wrapper">
-        <h2 class="related-prod-heading">All Subjects</h2>
-        <div class="med-rel-prod-slider-wrapper">
-            <div class="med-rel-prod-slider carousel-content">
-
-                <div class="med-product-card">
-                    <div class="related-prod-detail">
-                        <a href="subjectsFilter.php" class="rel-med-name">Mathematics</a>
-                    </div>
-                </div>
-
-                <div class="med-product-card">
-                    <div class="related-prod-detail">
-                        <a href="subjectsFilter.php" class="rel-med-name">Computer Science</a>
-                    </div>
-                </div>
-
-                <div class="med-product-card">
-                    <div class="related-prod-detail">
-                        <a href="subjectsFilter.php" class="rel-med-name">Literature</a>
-                    </div>
-                </div>
-
-                <div class="med-product-card">
-                    <div class="related-prod-detail">
-                        <a href="subjectsFilter.php" class="rel-med-name">History</a>
-                    </div>
-                </div>
-                <div class="med-product-card">
-                    <div class="related-prod-detail">
-                        <a href="subjectsFilter.php" class="rel-med-name">Biology</a>
-                    </div>
-                </div>
-
-                <div class="med-product-card">
-                    <div class="related-prod-detail">
-                        <a href="subjectsFilter.php" class="rel-med-name">Chemistry</a>
-                    </div>
-                </div>
-
+    <div class="background">
+        <img class="bkgr" src="/Images/backgr.jpg" alt="">
+        <div class="hero">
+            <div class="left">
+                <h1 class="title">Unlock your potential<br> with the best<br> univercity tutors.</h1>
+                <a class="submitInput" href="courses.php">
+                    <input class="submitBtn" type="submit" name="submit" value="Get Started">
+                </a>
             </div>
-
+            <div class="right">
+                <img class="banner" src="images/banner.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="info">
+        <div class="grid">
+            <div class="grid-item">
+                <div><img class="icon" src="/Images/star.png" alt=""></div>
+                <div>
+                    <h6>Expert teaching staff comprised of leading educators in various fields.</h6>
+                </div>
+            </div>
+            <div class="grid-item">
+                <div><img class="icon" src="/Images/person.png" alt=""></div>
+                <div>
+                    <h6>Personalized mentorship tailored to individual learning styles and needs.</h6>
+                </div>
+            </div>
+            <div class="grid-item">
+                <div><img class="icon" src="/Images/book.png" alt=""></div>
+                <div>
+                    <h6>Comprehensive study materials and resources to aid in exam preparation.</h6>
+                </div>
+            </div>
+            <div class="grid-item">
+                <div><img class="icon" src="/Images/seo.png" alt=""></div>
+                <div>
+                    <h6>Proven strategies and techniques for optimizing academic performance.</h6>
+                </div>
+            </div>
+            <div class="grid-item">
+                <div><img class="icon" src="/Images/calendar.png" alt=""></div>
+                <div>
+                    <h6>Flexible scheduling options to accommodate busy student lifestyles.</h6>
+                </div>
+            </div>
+            <div class="grid-item">
+                <div><img class="icon" src="/Images/flag.png" alt=""></div>
+                <div>
+                    <h6>Continuous support and guidance throughout the university admission process.</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="courses">
+            <h3 class="upcoming">Upcoming Courses</h3>
+            <div class="row row-cols-xl-3 row-cols-lg-3 row-cols-md-3 row-cols-sm-2 row-cols-1">
+                <?= $layout ?>
+            </div>
+            <a class="submitInput" href="courses.php">
+                <input class="courses-btn" type="submit" name="submit" value="All Courses">
+            </a>
         </div>
     </div>
 
-    <br>
-    <div class="med-related-prod-wrap" id="med-related-prod-wrapper">
-        <h2 class="related-prod-heading">All Universities</h2>
-        <div class="med-rel-prod-slider-wrapper">
-            <div class="med-rel-prod-slider carousel-content">
-
-                <div class="med-product-card">
-                    <div class="related-prod-detail">
-                        <a href="subjectsFilter.php" class="rel-med-name">University of Vienna</a>
-                    </div>
+    <div class="testimonials">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="..." alt="First slide">
                 </div>
-
-                <div class="med-product-card">
-                    <div class="related-prod-detail">
-                        <a href="subjectsFilter.php" class="rel-med-name">Vienna University of Technology</a>
-                    </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="..." alt="Second slide">
                 </div>
-
-                <div class="med-product-card">
-                    <div class="related-prod-detail">
-                        <a href="subjectsFilter.php" class="rel-med-name">University of Graz</a>
-                    </div>
-                </div>
-
-
-                <div class="med-product-card">
-                    <div class="related-prod-detail">
-                        <a href="subjectsFilter.php" class="rel-med-name">University of Innsbruck</a>
-                    </div>
-                </div>
-
-                <div class="med-product-card">
-                    <div class="related-prod-detail">
-                        <a href="subjectsFilter.php" class="rel-med-name">Medical University of Vienna</a>
-                    </div>
-                </div>
-                <div class="med-product-card">
-                    <div class="related-prod-detail">
-                        <a href="subjectsFilter.php" class="rel-med-name">University of Salzburg</a>
-                    </div>
-                </div>
-                <div class="med-product-card">
-                    <div class="related-prod-detail">
-                        <a href="subjectsFilter.php" class="rel-med-name">University of Klagenfurt </a>
-                    </div>
-                </div>
-                <div class="med-product-card">
-                    <div class="related-prod-detail">
-                        <a href="subjectsFilter.php" class="rel-med-name">Graz University of Technology</a>
-                    </div>
-                </div>
-                <div class="med-product-card">
-                    <div class="related-prod-detail">
-                        <a href="subjectsFilter.php" class="rel-med-name">Johannes Kepler University Linz</a>
-                    </div>
-                </div>
-                <div class="med-product-card">
-                    <div class="related-prod-detail">
-                        <a href="subjectsFilter.php" class="rel-med-name">University of Veterinary Medicine Vienna</a>
-                    </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="..." alt="Third slide">
                 </div>
             </div>
-
-            <br><br>
-            <div width="100%">
-
-                <div class="cont" width="100%">
-
-                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-5  grid" width="100%">
-                        <?= $layout ?>
-                    </div>
-                </div>
-
-                <script>
-                    const bindCarouselEvents = (containerId) => {
-                        const wrapper = document.getElementById(containerId);
-                        const btn_left = wrapper.getElementsByClassName('btn-left')[0]
-                        const btn_right = wrapper.getElementsByClassName('btn-right')[0]
-                        const content = wrapper.getElementsByClassName('carousel-content')[0]
-                        const content_scroll_width = content.scrollWidth;
-                        let content_scoll_left = content.scrollLeft;
-                        if (btn_right) {
-                            btn_right.addEventListener('click', () => {
-                                content_scoll_left += 224;
-                                if (content_scoll_left >= content_scroll_width) {
-                                    content_scoll_left = content_scroll_width;
-                                }
-                                content.scrollLeft = content_scoll_left;
-                            });
-                        }
-                        if (btn_left) {
-                            btn_left.addEventListener('click', () => {
-                                content_scoll_left -= 224;
-                                content.scrollLeft = content_scoll_left;
-                            });
-                        }
-
-                        // scroll binding
-                        content.addEventListener('scroll', () => {
-                            let scrollLeft = Math.ceil(content.scrollLeft)
-                            let contentScrollWidth = content.scrollWidth
-                            let contentWidth = content.clientWidth
-                            let rightEdge = (contentScrollWidth - contentWidth)
-                            if (scrollLeft >= rightEdge) {
-                                btn_right.style.display = "none"
-                            } else if (scrollLeft < rightEdge) {
-                                btn_right.style.display = "block"
-                            }
-
-                            if (scrollLeft == 0) {
-                                btn_left.style.display = "none"
-                            } else if (scrollLeft > 0) {
-                                btn_left.style.display = "block"
-                            }
-
-                            content_scoll_left = scrollLeft
-                        });
-                    }
-
-                    // javascript for scroll on click
-                    window.addEventListener('DOMContentLoaded', function() {
-                        bindCarouselEvents('med-related-prod-wrapper')
-                    });
-                </script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
