bd891951dd342472aabd945b6a6b15e490299ba3
Alex <alex23antonov@gmail.com>
2024-04-24 09:57:10 +0200

    courses

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar ">
+    <div class="container">
+        <a href="index.php">
+            <img class="nav-icon" src="images/logo.png" alt="">
+
+        </a>
+
+        <a class="navbar-brand" href="courses.php">
+            Courses
+        </a>
+        <a class="navbar-brand" href="universityFilter.php">
+            About Us
+        </a>
+        <a class="navbar-brand" href="updateprofile.php">
+            Contact Us
+        </a>
+
+        <a class="navbar-brand" href="../login/logout.php?logout">
+            Logout
+        </a>
+    </div>
+</nav>
