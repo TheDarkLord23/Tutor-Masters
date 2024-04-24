fb361c504a4b7deeb09b0025de71fb1cfd640a50
DacianBeniamin <sasdacian@gmx.at>
2024-04-24 14:08:02 +0200

    Trainer Index

--- Trainer/indexTrainer.php
+++ Trainer/indexTrainer.php
@@ -20,9 +20,14 @@ $layout = "";
 
 if (mysqli_num_rows($result) > 0) {
     foreach ($rows as $value) {
-        $layout .= "
-        <p>course type : {$value["subject"]}</p>
-        <img src='{$value["picture"]}' width='50'> ";
+        $layout .= "<div>
+        <div class='card my-3' style='width: 20 rem;'>
+        <img src='{$value["picture"]}' class='card-img-top' alt='...'>
+        <div class='card-body'>
+          <h5 class='card-title'>{$value["subject"]}</h5>
+        </div>
+      </div>
+    </div>";
     }
 } else {
     $layout = "no courses found!";
@@ -35,13 +40,18 @@ if (mysqli_num_rows($result) > 0) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Hello <?= $row["first_name"] ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
+    <a class="btn btn-primary" href="../User/indexUser.php" style="margin-left: 70px; margin-top: 50px;">Back</a>
     <div class="row row-cols-lg-3 row-cols-md-2 row-cols-xs-1" style="margin-left: 70px;">
         <?= $layout ?>
     </div>
 </body>
 
 </html>
+
+<a href='crud/details.php?id={$value["id"]}' class='btn btn-success'>More Details</a>
+<a href='adopte.php?id={$value["id"]}' class='btn btn-success'>Take Me Home</a>
