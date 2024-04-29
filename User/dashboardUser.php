83b32ddc8cfd755bf75acaeb59f78cad5366b1be
Alex <alex23antonov@gmail.com>
2024-04-29 10:48:36 +0200

    dash

--- User/dashboardUser.php
+++ User/dashboardUser.php
@@ -42,38 +42,39 @@ if (!$result) {
 <body>
     <div class="dashboard-container">
         <div class="left-section">
-            <a href="dashboardUser.php" class="action action-active">
-                <div class="icon"></div>
-                <div class="title">Action 1</div>
+            <a href="dashboardUser.php" class="action action-active" id="dashboardBtn">
+                <img class="icon" src="../Images/house.png" alt="">
+                <div class="title">Dashboard</div>
             </a>
-            <a href="action2.php" class="action">
+            <a href="#" class="action" id="action2Btn">
                 <div class="icon"></div>
                 <div class="title">Action 2</div>
             </a>
-            <a href="action3.php" class="action">
+            <a href="#" class="action" id="action3Btn">
                 <div class="icon"></div>
                 <div class="title">Action 3</div>
             </a>
-            <a href="action4.php" class="action">
+            <a href="#" class="action" id="action4Btn">
                 <div class="icon"></div>
                 <div class="title">Action 4</div>
             </a>
         </div>
-        <div class="center-section">
-            <h2 class="dashboard-title">Dashboard</h2>
-            <div id="current-date"></div>
-            <!-- <div class="navigation"></div> -->
-            <div>
-                <div class="hello">
-                    <img class="graphs" src="../Images/graphs.png" alt="">
-                    <div class="hello-right">
-                        <h2>Hello, <?php echo $user['firstName'] . ' ' . $user['secondName']; ?></h2>
-                        <h5>This is your Dashboard. Here you can see all your info. You can edit your Profile. View and Edit your Courses. View and Edit your Reviews. You can see your Calendar and change the dates for a Course you have already signed up for.</h5>
+        <div class="center-section" id="centerSection">
+            <div class="center-left">
+                <h2 class="dashboard-title">Dashboard</h2>
+                <div id="current-date"></div>
+                <!-- <div class="navigation"></div> -->
+                <div>
+                    <div class="hello">
+                        <img class="graphs" src="../Images/graphs.png" alt="">
+                        <div class="hello-right">
+                            <h2>Hello, <?php echo $user['firstName'] . ' ' . $user['secondName']; ?></h2>
+                            <h5>This is your Dashboard. Here you can see all your info. You can edit your Profile. View and Edit your Courses. View and Edit your Reviews. You can see your Calendar and change the dates for a Course you have already signed up for.</h5>
+                        </div>
                     </div>
-                </div>
-                <div class="content">
-                    <div class="items-left">
-                        <!-- <div class="grid">
+                    <div class="content">
+                        <div class="items-left">
+                            <!-- <div class="grid">
                             <div class="box">
                                 <div class="icon">Icon 1</div>
                                 <div class="text">Text 1</div>
@@ -91,29 +92,31 @@ if (!$result) {
                                 <div class="text">Text 4</div>
                             </div>
                         </div> -->
-                        <div class="box">
-                            <img src="../Images/star-full.png" class="icon-big" alt="">
-                            <h2 class="text">My Reviews</h2>
+                            <div class="box">
+                                <img src="../Images/star-full.png" class="icon-big" alt="">
+                                <h2 class="text">My Reviews</h2>
+                            </div>
+                            <div class="box">
+                                <img src="../Images/open-book.png" class="icon-big" alt="">
+                                <h2 class="text">My Courses</h2>
+                            </div>
                         </div>
-                        <div class="box">
-                            <img src="../Images/open-book.png" class="icon-big" alt="">
-                            <h2 class="text">My Courses</h2>
-                        </div>
-                    </div>
-                    <div class="items-right">
+                        <div class="items-right">
 
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="right-section">
-            <div class="profile-info">
-                <?= $layout ?>
-                <a class="updateInput" href="courses.php">
-                    <input class="updateBtn" type="submit" name="update" value="Update Profile">
-                </a>
+            <div class="center-right">
+                <div class="profile-info">
+                    <?= $layout ?>
+                    <a class="updateInput" href="courses.php">
+                        <input class="updateBtn" type="submit" name="update" value="Update Profile">
+                    </a>
+                </div>
             </div>
         </div>
+
     </div>
 
     <script>
@@ -128,9 +131,52 @@ if (!$result) {
         var formattedDate = currentDate.toLocaleDateString('en-US', options);
 
         document.getElementById('current-date').textContent = formattedDate;
+
+        // Function to handle dashboard button click (refresh page)
+        document.getElementById('dashboardBtn').addEventListener('click', function(event) {
+            location.reload();
+        });
+
+        document.addEventListener("DOMContentLoaded", function() {
+            // Get reference to center section and action buttons
+            var centerSection = document.getElementById("centerSection");
+            var actionButtons = document.querySelectorAll(".action");
+
+            // Function to handle button click
+            function handleButtonClick(event) {
+                event.preventDefault();
+
+                // Check if the clicked button is not already active
+                if (!this.classList.contains("action-active")) {
+                    // Remove action-active class from all buttons
+                    actionButtons.forEach(function(btn) {
+                        btn.classList.remove("action-active");
+                    });
+
+                    // Add action-active class to the clicked button
+                    this.classList.add("action-active");
+
+                    // Get the href attribute of the clicked button
+                    var href = this.getAttribute("href");
+
+                    // Fetch the content from the href
+                    fetch(href)
+                        .then(response => response.text())
+                        .then(data => {
+                            // Update the center section with the fetched content
+                            centerSection.innerHTML = data;
+                        })
+                        .catch(error => console.error("Error:", error));
+                }
+            }
+
+            // Add click event listeners to action buttons
+            actionButtons.forEach(function(btn) {
+                btn.addEventListener("click", handleButtonClick);
+            });
+        });
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
 </body>
 
 </html>
