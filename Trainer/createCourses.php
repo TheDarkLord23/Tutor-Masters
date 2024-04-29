1dc8205b747fbbbf4c18aa08e56fbb7927523cd4
Anna Hempel <annahempel@gmx.net>
2024-04-29 13:38:30 +0200

    every location fixed to dashboard

--- Trainer/createCourses.php
+++ Trainer/createCourses.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION["admin"]) && !isset($_SESSION["trainer"])) {
 }
 
 if (isset($_SESSION["user"])) {
-    header("Location: ../User/indexUser.php");
+    header("Location: ../User/dashboardUser.php");
 }
 
 // validation end
