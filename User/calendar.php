ab7026dc847065a93f2e392d187fa13c8c704403
Anna Hempel <annahempel@gmx.net>
2024-04-30 13:03:22 +0200

    calendar

--- User/calendar.php
+++ User/calendar.php
@@ -60,6 +60,10 @@ $result = mysqli_query($connection,$sql);
         text-decoration: none;
         color: #565656;
       }
+      a#fc_dom_14{
+        text-decoration: none;
+        color: #565656;
+      }
     </style>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
